e36b3ebd971c71a0c41f9e4efd0682fca2b2921d
Hassan Azad <zainsatti1901@gmail.com>
2023-07-04 13:24:52 +0500

    HTML/CSS/JS

--- JS/nav.js
+++ JS/nav.js
@@ -1,14 +1,24 @@
-const bar = document.getElementById("bar");
-const nav = document.getElementById("navbar1");
-const close = document.getElementById("close");
+// function myFunction() {
+//   var x = document.getElementById("navbar");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+// Get the hamburger icon and the nav_links element
+// const hamburgerIcon = document.querySelector(".icon");
+// const navLinks = document.querySelector(".nav_links");
 
-if (bar) {
-  bar.addEventListener("click", () => {
-    nav.classList.add("active");
-  });
-}
-if (close) {
-  close.addEventListener("click", () => {
-    nav.classList.remove("active");
-  });
-}
+// // Add a click event listener to the hamburger icon
+// hamburgerIcon.addEventListener("click", function () {
+//   // Toggle the active class on the nav_links element
+//   navLinks.classList.toggle("active");
+// });
+
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show_nav");
+});
