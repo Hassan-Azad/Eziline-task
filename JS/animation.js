1faea065f0862d9768f63cf1bd6ceed74011a31e
Hassan Azad <zainsatti1901@gmail.com>
2023-07-05 13:29:17 +0500

    Portfolio

--- JS/animation.js
+++ JS/animation.js
@@ -1,15 +1,20 @@
-window.addEventListener("scroll", function () {
-  var elements = document.querySelectorAll(".project_img img");
-
-  for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    var position = element.getBoundingClientRect();
-
-    // If the element is in the viewport
-    if (position.top < window.innerHeight) {
-      element.style.transform = "rotate(0deg)"; // Show the element
+// animation
+function reveal() {
+  var reveals = document.querySelectorAll(
+    ".phone, .Email, .content h6, .content, .form, .logo_header, .main_nav li button, .main_nav li i, .img img, .intro, .box1, .box2, .objective, .project_des > .content,.project_img img "
+  );
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
     } else {
-      element.style.transform = "rotate(-10deg)"; // Reset the element to its original position
+      reveals[i].classList.remove("active");
     }
   }
-});
+}
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
--- JS/nav.js
+++ JS/nav.js
@@ -1,24 +1,23 @@
-// function myFunction() {
-//   var x = document.getElementById("navbar");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-// Get the hamburger icon and the nav_links element
-// const hamburgerIcon = document.querySelector(".icon");
-// const navLinks = document.querySelector(".nav_links");
-
-// // Add a click event listener to the hamburger icon
-// hamburgerIcon.addEventListener("click", function () {
-//   // Toggle the active class on the nav_links element
-//   navLinks.classList.toggle("active");
-// });
-
 const navToggle = document.querySelector(".navbar_toggle");
 const links = document.querySelector(".main_nav");
 
 navToggle.addEventListener("click", function () {
   links.classList.toggle("show_nav");
+});
+
+// avtive page
+
+// Get the current page URL
+var currentPageUrl = window.location.href;
+
+// Get all the navigation links
+var navLinks = document.querySelectorAll(".main_nav li a");
+
+// Iterate over each navigation link
+navLinks.forEach(function (link) {
+  // Check if the link's href matches the current page URL
+  if (link.href === currentPageUrl) {
+    // Add the "nav-active" class to the active link
+    link.classList.add("nav-active");
+  }
 });
